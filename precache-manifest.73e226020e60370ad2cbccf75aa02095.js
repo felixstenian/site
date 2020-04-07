b30162c4c5ad1912a494cfbcfd62e470cb2164e2
Stenia Felix <steniafelix@gmail.com>
2020-04-06 21:12:26 -0300

    Updates

---
+++ precache-manifest.73e226020e60370ad2cbccf75aa02095.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e44acc388a968c206d9c4a83fc25ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b03e9de03200120f6555",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71037645f72f741fd4ad",
+    "url": "/static/js/2.04311b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04311b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03e9de03200120f6555",
+    "url": "/static/js/main.78538eed.chunk.js"
+  },
+  {
+    "revision": "e0c5a4a5e778671927e0",
+    "url": "/static/js/runtime-main.01bf120f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
