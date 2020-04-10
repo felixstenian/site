56ab420965d38aa5d65b5dacdbe402a5979737bb
Stenia Felix <steniafelix@gmail.com>
2020-04-10 19:07:56 -0300

    Adicionando rotas e styles global

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import Routes from "./routes";
+import GlobalStyle from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Felix</p>
-      </header>
-    </div>
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
