80c5ad9fc72dbad79adb5d490293fcc707779e12
felixstenian <felixstenian@gmail.com>
2022-02-09 20:35:05 -0300

    Updates

---
+++ precache-manifest.239b81a5ca96ad3d3eb41f09d84d2aac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24ed5a340142565510c51139db6ee67",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "c3393131fc9a2462231f",
+    "url": "/site/static/js/2.f0bc9c8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site/static/js/2.f0bc9c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed6adeec887a7dd79bc",
+    "url": "/site/static/js/main.a62bd3e1.chunk.js"
+  },
+  {
+    "revision": "e714eb906535f502d1fc",
+    "url": "/site/static/js/runtime-main.e99dfbaf.js"
+  }
+]);
